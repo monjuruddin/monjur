fa0cbc8c532768d5db6ac3ad91fcc1069b555020
monjuruddin <90463942+monjuruddin@users.noreply.github.com>
2021-10-20 10:50:25 +0600

    Add files via upload

---
+++ responsiv.js
@@ -0,0 +1,13 @@
+
+
+// Javascript for toggle menu
+
+let navlinks = document.getElementById("navlinks")
+
+function showmenu(){
+    navlinks.style.right = "0"
+}
+
+function hidemenu(){
+    navlinks.style.right = "-200px"
+}
